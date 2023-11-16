bc9172aaf21dedb51fd0d2a9f7f0ea2feef1bf84
wuE0853 <115360087+wuE0853@users.noreply.github.com>
2023-11-16 11:47:02 +0800

    Update setcal.py
    add caculation of mse

--- setcal.py
+++ setcal.py
@@ -1,15 +1,15 @@
 import os
 import calculate
 
-hr_path='/root/autodl-tmp/valid_HR'
-sr_path='/root/autodl-tmp/valid_SR'
+hr_path='/root/autodl-tmp/test_1024'
+sr_path='/root/autodl-tmp/DATSR'
 hr_set=os.listdir(hr_path)
 sr_set=os.listdir(sr_path)
 hr_set.sort()
 sr_set.sort()
 scale= len(hr_set)
 
-output=[0,0]
+output=[0,0,0]
 
 for i in range(scale):
     name_hr, ext_hr = hr_set[i].split('.')
@@ -18,6 +18,8 @@ for i in range(scale):
         result= calculate.image(hr_path+'/'+hr_set[i], sr_path+'/'+sr_set[i])
         output[0]+=result[0]
         output[1]+=result[1]
+        output[2]+=result[2]
     
 print('psnr='+str(output[0]/scale))
 print('ssim='+str(output[1]/scale))
+print('mse='+str(output[2]/scale))
